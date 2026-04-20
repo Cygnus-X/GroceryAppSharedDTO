fd7e95f4ae29d7b3ef12676f2fdfe8d7ad6e835e
Viktor Kaderabek <kaderabek.viktor@seznam.cz>
2026-04-20 22:07:19 +0200

    register and login response

---
+++ Sources/GroceryAppSharedDTO/GroceryAppSheredDTO.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Viktor Kaderabek on 20.04.2026.
+//
+
+import Foundation
---
+++ Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
@@ -0,0 +1,24 @@
+//
+//  LoginResponseDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Viktor Kaderabek on 20.04.2026.
+//
+
+import Foundation
+
+public struct LoginResponseDTO: Codable {
+    
+    public let error: Bool
+    public var reason: String? = nil
+    public var token: String? = nil
+    public var userId: UUID? = nil
+    
+    public init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil) {
+        
+        self.error = error
+        self.reason = reason
+        self.token = token
+        self.userId = userId
+    }
+}
