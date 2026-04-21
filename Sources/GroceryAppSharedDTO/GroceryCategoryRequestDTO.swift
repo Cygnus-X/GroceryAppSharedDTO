f880d38ddace3981ea8c89f658b6b08274bcbcc7
Viktor Kaderabek <kaderabek.viktor@seznam.cz>
2026-04-21 22:59:41 +0200

    updating model

--- Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  GroceryCategoryRequestDTO.swift
 //  GroceryAppSharedDTO
 //
 //  Created by Viktor Kaderabek on 21.04.2026.
@@ -8,13 +8,13 @@
 import Foundation
 
 public struct GroceryCategoryRequestDTO: Codable {
-    internal init(title: String, colorCode: String) {
+
+    public let title: String
+    public let colorCode: String
+
+    public init(title: String, colorCode: String) {
+        
         self.title = title
         self.colorCode = colorCode
     }
-    
-    
-    public let title: String
-    public let colorCode: String
-    
 }
--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -1,5 +1,5 @@
 //
-//  GroceryCategoryRequestDTO 2.swift
+//  GroceryCategoryResponseDTO.swift
 //  GroceryAppSharedDTO
 //
 //  Created by Viktor Kaderabek on 21.04.2026.
@@ -8,13 +8,15 @@
 
 import Foundation
 
-public struct GroceryCategoryRequestDTO: Codable {
+public struct GroceryCategoryResponseDTO: Codable {
 
+    public let id: UUID
     public let title: String
     public let colorCode: String
 
-    public init(title: String, colorCode: String) {
+    public init(id: UUID, title: String, colorCode: String) {
         
+        self.id = id
         self.title = title
         self.colorCode = colorCode
     }
