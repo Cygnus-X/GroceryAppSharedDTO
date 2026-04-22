71dc97dd1200eebb5ff7a42d02dc27fae93daac0
Viktor Kaderabek <kaderabek.viktor@seznam.cz>
2026-04-22 14:49:09 +0200

    new target and library

--- Package.swift
+++ Package.swift
@@ -11,6 +11,10 @@ let package = Package(
             name: "GroceryAppSharedDTO",
             targets: ["GroceryAppSharedDTO"]
         ),
+        .library(
+            name: "GroceryAppSharedEndpoints",
+            targets: ["GroceryAppSharedEndpoints"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,6 +25,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GroceryAppSharedDTO",
+            dependencies: []
+        ),
+        .target(
+            name: "GroceryAppSharedEndpoints",
             dependencies: []
         )
     ]
